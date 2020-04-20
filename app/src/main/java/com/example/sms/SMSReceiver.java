bc9fb523df4ca4fa095bf714f5781c017f1faac1
irfanswr <irfan.siswara.tif18@polban.ac.id>
2020-04-20 12:27:27 +0700

    receiving sms

--- app/src/main/java/com/example/sms/SMSReceiver.java
+++ app/src/main/java/com/example/sms/SMSReceiver.java
@@ -11,25 +11,31 @@ import android.widget.Toast;
 
 public class SMSReceiver extends BroadcastReceiver {
     @Override
-    public void onReceive(Context context, Intent intent)
-    {
+    public void onReceive(Context context, Intent intent) {
         //---get the SMS message passed in---
         Bundle bundle = intent.getExtras();
         SmsMessage[] msgs = null;
         String str = "SMS from ";
-        if (bundle != null)
-        {
-    //---retrieve the SMS message received---
-            msgs = Telephony.Sms.Intents.getMessagesFromIntent(intent);
-            for (int i=0; i<msgs.length; i++){
+        if (bundle != null) {
+            //---retrieve the SMS message received---
+            Object[] pdus = (Object[]) bundle.get("pdus");
+            msgs = new SmsMessage[pdus.length];
+            for (int i = 0; i < msgs.length; i++) {
+                msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                if (i == 0) {
+//---get the sender address/phone number---
+                    str += msgs[i].getOriginatingAddress();
+                    str += ": ";
+                }
+//---get the message body---
                 str += msgs[i].getMessageBody().toString();
             }
-    //---get the message body---
-            str += msgs[i].getMessageBody().toString();
+//---display the new SMS message---
+            Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
+            Log.d("SMSReceiver", str);
+//---stop the SMS message from being broadcasted---
+            this.abortBroadcast();
         }
-    //---display the new SMS message---
-        Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
-        Log.d("SMSReceiver", str);
     }
 }
 
